3eccc08fb95d71a1cf5f4e1c4854e758c8984858
AdamNaghs <adamnaghavif@gmail.com>
2025-05-06 16:01:32 -0400

    Updated

--- tests/plugin_api.c
+++ tests/plugin_api.c
@@ -2,39 +2,38 @@
 #include "raylib.h"
 #include <stdio.h>
 
-int init(CoreContext* ctx)
+int init(CoreContext *ctx)
 {
-    InitWindow(800,600,"Test");
+    InitWindow(800, 600, "Test");
     return 0;
 }
 
-int update(CoreContext* ctx)
+int update(CoreContext *ctx)
 {
     if (WindowShouldClose() || IsKeyPressed(KEY_SPACE))
     {
-        int *ptr = CTX_GET(ctx,"CORE_SHOULD_RUN");
+        int *ptr = CC_GET(ctx, "CORE_SHOULD_RUN");
         *ptr = 0;
     }
-    return 0;
-}
 
-int draw(CoreContext* ctx)
-{
     BeginDrawing();
     ClearBackground(WHITE);
-    DrawText("Hello World!",400,300,20,BLACK);
-    DrawFPS(0,0);
+    DrawText("Hello World!", 400, 300, 20, BLACK);
+    DrawFPS(0, 0);
     EndDrawing();
     return 0;
 }
 
-int shutdown(CoreContext* ctx)
+int shutdown(CoreContext *ctx)
 {
     CloseWindow();
     return 0;
 }
 
+static const char* deps[] = { "Scheduler", NULL };
+static PluginMetadata meta = {"Plugin Test", deps};
+
 PluginAPI Load()
 {
-    return (PluginAPI){init,update,draw,shutdown};
+    return (PluginAPI){init, update, shutdown, &meta};
 }
