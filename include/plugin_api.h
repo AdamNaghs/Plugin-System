faf547c0e74bf5ac95b4aae0f55544419f6a773a
AdamNaghs <adamnaghavif@gmail.com>
2025-05-06 16:00:42 -0400

    Added metadata and removed draw

--- include/plugin_api.h
+++ include/plugin_api.h
@@ -2,15 +2,16 @@
 #define _PLUGIN_API_H
 
 #include "core_context.h"
+typedef struct PluginMetadata {
+    const char* name;
+    const char** depends_on;  // NULL-terminated list
+} PluginMetadata;
 
-typedef int (*Plugin_Function)(CoreContext*);
-
-typedef struct PluginAPI
-{
+typedef struct PluginAPI {
     int (*init)(CoreContext*);
     int (*update)(CoreContext*);
-    int (*draw)(CoreContext*);
     int (*shutdown)(CoreContext*);
+    PluginMetadata* meta;  // NEW
 } PluginAPI;
 
 /* The user define this function to return the struct above */
