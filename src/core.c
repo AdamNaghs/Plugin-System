7a176c1d88733d02fcac142068bd91204f656b60
AdamNaghs <adamnaghavif@gmail.com>
2025-05-06 16:01:44 -0400

    Renamed macros

--- src/core.c
+++ src/core.c
@@ -6,7 +6,7 @@ void core_init(Core* core, int version, char* plugin_folder)
 {
     core_context_new(&core->context,version);
     plugin_manager_new(&core->manager,plugin_folder);
-    int *should_run = CTX_ALLOC(&core->context,"CORE_SHOULD_RUN",sizeof(int));
+    int *should_run = CC_ALLOC(&core->context,"CORE_SHOULD_RUN",sizeof(int));
     *should_run = 1;
     plugin_manager_init(&core->manager,&core->context);
     
@@ -15,11 +15,10 @@ void core_init(Core* core, int version, char* plugin_folder)
 
 void core_run(Core* core)
 {
-    int *should_run = CTX_GET(&core->context,"CORE_SHOULD_RUN");
+    int *should_run = CC_GET(&core->context,"CORE_SHOULD_RUN");
     while (should_run && *should_run) {
         core_context_update(&core->context);
         plugin_manager_update(&core->manager,&core->context);
-        plugin_manager_draw(&core->manager,&core->context);
     }
 }
 
