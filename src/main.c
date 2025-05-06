5ec1e6e2963a2bc52877e2c5f8acdaccc91a40ca
AdamNaghs <adamnaghavif@gmail.com>
2025-05-06 17:33:16 -0400

    Added command line input

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include "../include/core.h"
+#include <stdio.h>
 
-int main(void)
+int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <plugin_folder>\n", argv[0]);
+        return 1;
+    }
+
     Core c;
-    core_init(&c,0,"/Users/adamnaghavi/Dropbox/src/c_code/core_plugin/build/plugins");
+    core_init(&c, 0, argv[1]);
     core_run(&c);
     core_shutdown(&c);
     return 0;
