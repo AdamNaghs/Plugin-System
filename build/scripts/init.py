93288da6332168329874ca075268b4b3dc1d14cf
AdamNaghs <adamnaghavif@gmail.com>
2025-05-11 13:13:15 -0400

    Updated test script

--- build/scripts/init.py
+++ build/scripts/init.py
@@ -12,29 +12,29 @@ my_sensor = Sensor("FrontDistanceSensor")
 def on_obstacle_detected(sender, args):
     # sender will be the Sensor object
     if sender:
-        core.log(f"[python] Signal received from: {sender}")
+        core.log(f"Signal received from: {sender}")
 
     # args will be a dictionary with more information
     if not args:
-        core.log("[python] No args provided with signal.")
+        core.log("No args provided with signal.")
         return
     distance = args.get("distance", None)
     if distance == None:
-        core.log("[python] No distance provided in args.")
+        core.log("No distance provided in args.")
         return
-    core.log(f"[python] Obstacle detected at {distance} cm!")
+    core.log(f"Obstacle detected at {distance} cm!")
     if distance < 10:
-        core.log("[python] Distance too close! Emitting 'move_backward' signal.")
+        core.log("Distance too close! Emitting 'move_backward' signal.")
         core.signal_emit("move_backward", None, None)
 
 def init():
-    core.log("[python] Running init()")
+    core.log("Running init()")
 
     # Connect callback
     core.signal_connect("obstacle_detected", on_obstacle_detected)
 
     # Immediately emit obstacle_detected signal for testing
-    core.log("[python] Emitting 'obstacle_detected' signal with sender and args...")
+    core.log("Emitting 'obstacle_detected' signal with sender and args...")
     core.signal_emit(
         "obstacle_detected",
         my_sensor,                 # sender = our sensor object
@@ -45,4 +45,4 @@ def update(dt):
     pass
 
 def shutdown():
-    core.log("[python] Running shutdown()")
+    core.log("Running shutdown()")
